84850f141f5e75f35a72398469d7d2ff89ebaf04
fjesser <felixjonas.esser@gmail.com>
2025-02-24 15:40:29 -0500

    Update package and license information

--- ox-beamer-lecture.el
+++ ox-beamer-lecture.el
@@ -1,13 +1,28 @@
 ;;; ox-beamer-lecture.el --- Beamer Lecture Back-End for Org Export Engine -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2007-2024 Free Software Foundation, Inc.
+;; Copyright (C) 2025 Felix J. Esser
 
 ;; Author: Felix J. Esser <code-esser@mailbox.org>
 ;; Maintainer: Felix J. Esser <code-esser@mailbox.org>
-;; Created: 2025
+;; URL https://github.com/fjesser/ox-beamer-lecture;; Created: 2025
 ;; Version: 1.0
-;; Homepage: https://github.com/fjesser/ox-beamer-lecture
+;; Package-Requires: ((emacs "26.1") (org "9.6"))
 ;; Keywords: org, text, tex
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
